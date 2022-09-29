ce550e8006de7c4ec855bacff4278a99a19b90ab
Sahilcoder4 <112623861+Sahilcoder4@users.noreply.github.com>
2022-09-29 17:45:59 +0530

    Update currencyconverter.java

--- currencyconverter.java
+++ currencyconverter.java
@@ -6,7 +6,7 @@ public class currencyconverter
    public static void main(String[] args)
    {
       double amount;
-      double rupee, dollar, pound, euro, yen, ringgit;
+      double rupee, dollar, pound, euro;
       int choice;
  
       DecimalFormat f = new DecimalFormat("##.##");
@@ -18,8 +18,6 @@ public class currencyconverter
       System.out.println("Enter 2: Dollar");
       System.out.println("Enter 3: Pound");
       System.out.println("Enter 4: Euro");
-      System.out.println("Enter 5: Yen");
-      System.out.println("Enter 5: Ringgit");
  
       System.out.print("\nChoose from above options: ");
       choice = sc.nextInt();
@@ -38,12 +36,6 @@ public class currencyconverter
  
             euro = amount / 80;
             System.out.println(amount + " Rupee = " + f.format(euro) + " Euro");
- 
-            yen = amount / 0.63;
-            System.out.println(amount + " Rupee = " + f.format(yen) + " Yen");
- 
-            ringgit = amount / 16;
-            System.out.println(amount + " Rupee = " + f.format(ringgit) + " ringgit");
             break;
  
          case 2:  // Dollar Conversion
@@ -55,12 +47,6 @@ public class currencyconverter
  
             euro = amount *0.87;
             System.out.println(amount + " Dollar = " + f.format(euro) + " Euro");
- 
-            yen = amount *111.087;
-            System.out.println(amount + " Dollar = " + f.format(yen) + " Yen");
- 
-            ringgit = amount *4.17;
-            System.out.println(amount + " Dollar = " + f.format(ringgit) + " ringgit");
             break;
  
          case 3:  // Pound Conversion
@@ -72,13 +58,7 @@ public class currencyconverter
  
             euro = amount *1.10;
             System.out.println(amount + " pound = " + f.format(euro) + " Euro");
- 
-            yen = amount *140.93;
-            System.out.println(amount + " pound = " + f.format(yen) + " Yen");
- 
-            ringgit = amount *5.29;
-            System.out.println(amount + " pound = " + f.format(ringgit) + " ringgit");
-            break;
+             break;
  
          case 4:  // Euro Conversion
             rupee = amount * 80;
@@ -89,48 +69,9 @@ public class currencyconverter
  
             pound = amount *0.90;
             System.out.println(amount + " euro = " + f.format(pound) + " Pound");
- 
-            yen = amount *127.32;
-            System.out.println(amount + " euro = " + f.format(yen) + " Yen");
- 
-            ringgit = amount *4.78;
-            System.out.println(amount + " euro = " + f.format(ringgit) + " ringgit");
             break;
  
-         case 5:  // Yen Conversion
-            rupee = amount *0.63;
-            System.out.println(amount + " yen = " + f.format(rupee) + " Ruppes");
- 
-            dollar = amount *0.008;
-            System.out.println(amount + " yen = " + f.format(dollar) + " Dollar");
- 
-            pound = amount *0.007;
-            System.out.println(amount + " yen = " + f.format(pound) + " Pound");
- 
-            euro = amount *0.007;
-            System.out.println(amount + " yen = " + f.format(euro) + " Euro");
- 
-            ringgit = amount *0.037;
-            System.out.println(amount + " yen = " + f.format(ringgit) + " ringgit");
-            break;
- 
-         case 6:  // Ringgit Conversion
-            rupee = amount *16.8;
-            System.out.println(amount + " ringgit = " + f.format(rupee) + " Ruppes");
- 
-            dollar = amount *0.239;
-            System.out.println(amount + " ringgit = " + f.format(dollar) + " dollar");
- 
-            pound = amount *0.188;
-            System.out.println(amount + " ringgit =: " + f.format(pound) + " pound");
- 
-            euro = amount *0.209;
-            System.out.println(amount + " ringgit = " + f.format(euro) + " euro");
- 
-            yen = amount *26.63;
-            System.out.println(amount + " ringgit = " + f.format(yen) + " yen");
-            break;
- 
+         
           //Default case
          default:
             System.out.println("Invalid Input");
